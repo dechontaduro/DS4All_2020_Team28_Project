2fd3f342900a6d8c9c11a59cd88959341d201ee8
Juan Carlos González Cardona <juancg78@hotmail.com>
2020-07-02 00:54:31 -0500

    Data: - Actualización de las variables mensuales. - Eliminación de las gráficas exploratorias preliminares.

--- obtain/unir_csv.py
+++ obtain/unir_csv.py
@@ -2,13 +2,17 @@ import pandas as pd
 import glob
 
 def unzipfolder(path, output):
-	files = glob.glob(path + "\\*.zip")
+	files = glob.glob(path)
 	print(f"Leyendo {len(files)} archivos...")
 	l = [pd.read_csv(filename) for filename in files]
 	print("Concatenando...")
 	df = pd.concat(l, axis=0)
+#	df.drop(["i"], axis="columns", inplace = True)
+	
+#	df.drop_duplicates('codigo', inplace = True)
+    
 	print("Exportando...")
-	df.to_csv(path + "\\" + output)
+	df.to_csv(output,  index = False)
 
 """
 l = [filename for filename in glob.glob(path + "\\*.zip")]
@@ -16,8 +20,8 @@ print(path + "\\*.zip")
 print(*l, sep = "\n")
 """
 
-path = r"C:\Users\juanc\Google Drive\DS4All_Team28\Listado Estaciones IDEAM 1990-2020\data"
-output = "est_limnimetrica.csv"
+path = r"C:\Users\juanc\Source\Repos\DS4All_2020_Team28_Project\data\variables_profiles\CUM70_*.csv"
+output = r"C:\Users\juanc\Source\Repos\DS4All_2020_Team28_Project\data\variables_profiles\CUM70_todas.csv"
 #TEST
 
 print(f"Directorio:{path}")
